f76506b659a3601bad60acfff1ea384e0bcdf9c9
fengyaoo <fengyao.li@bc.edu>
2021-04-28 18:35:04 -0400

    Wed

--- precip_data_load.m
+++ precip_data_load.m
@@ -1,27 +1,30 @@
 %2008-2013
-filename = '2008_2013_la_county_qpcp.csv';
+filename = 'newprecipdata.csv';
 precip = readtable(filename);
 
-date = datetime(precip.DATE, 'inputformat','yyyyMMdd HH:mm');
+date = datetime(precip.DATE, 'inputformat','yyyyMMdd');
 date = datenum(date);
-station = precip.STATION_NAME;
+%station = precip.STATION_NAME;
 elevation =precip.ELEVATION;
 lat = precip.LATITUDE;
 lon = precip.LONGITUDE;
-qpcp = precip.QPCP;
-qgag = precip.QGAG;
+% qpcp = precip.QPCP;
+% qgag = precip.QGAG;
+rain = precip.PRCP; %unit 1/10 mm 
 
-precipitation = [date, elevation, lat, lon, qgag];
-precipitation = sortrows(precipitation, 1);
+scatter(date, rain);
 
-%QGAG most likely in tenth of millimeters
-%%
-ind = find(precipitation(:,5) == -9999 | precipitation(:,5)>1000); 
-%remove data with null value or unusable data points
-
-precipitation(ind,5) = NaN;
-
-%%
-figure (1); clf
-scatter(precipitation(:,1),precipitation(:,5))
-datetick('x','yyyy-MM-dd')
+% precipitation = [date, elevation, lat, lon, rain];
+% precipitation = sortrows(precipitation, 1);
+% 
+% %QGAG most likely in tenth of millimeters
+% %%
+% ind = find(precipitation(:,5) == -9999 | precipitation(:,5)>1000); 
+% %remove data with null value or unusable data points
+% 
+% precipitation(ind,5) = NaN;
+% 
+% %%
+% figure (1); clf
+% scatter(precipitation(:,1),precipitation(:,5))
+% datetick('x','yyyy-MM-dd')
