0ebab1adeb2dbb7760497edaf03bb8871d5d6b59
mingyuema730 <78006098+mingyuema730@users.noreply.github.com>
2021-04-30 18:17:22 -0400

    pm_precip_graph_completed

--- pm_usc_load.m
+++ pm_usc_load.m
@@ -12,5 +12,40 @@ lon_precip = -118.2911; %lat & lon for dtla precip station
 lat_pm = pm_raw.latitude(1);
 lon_pm = pm_raw.longitude(1); % lat & lon for dtla pm2.5 monitoring site
 
-d = sqrt( (lat_precip - lat_pm)^2 + (lon_precip - lon_pm)^2) * 111 %distance between two sites in km
+d = sqrt( (lat_precip - lat_pm)^2 + (lon_precip - lon_pm)^2) * 111; %distance between two sites in km
 
+%% find time in serial number and hourly 2.5 data & exploratory graph
+time = datenum(pm_raw.date + hours(pm_raw.start_hour));
+hourly_pm = pm_raw.value;
+hourly_pm_smooth = movmean(hourly_pm, 24);
+
+figure (1); clf
+plot(time, hourly_pm)
+datetick('x', 'mm-dd')
+
+%% Janurary pm2.5 & precip data
+ind_jan_pm = find(time >= datenum('2013-01-01 00:00:00') & time < datenum('2013-02-01 00:00:00'));
+ind_jan_event = find(precip_event.start_time > datenum('01-01-2013 00:00:00') & precip_event.start_time < datenum('02-01-2013 00:00:00'));
+
+figure (2); clf
+t = tiledlayout(1,1);
+ax1 = axes(t);
+p = plot(ax1, time(ind_jan_pm),hourly_pm_smooth(ind_jan_pm),'-b');
+xlabel('Date');
+ylabel('PM 2.5 Concentration (ug/m^3)');
+datetick('x','mm-dd');
+xlim(ax1, [datenum('12-31-2012 23:59:59') datenum('02-01-2013 00:00:00')]);
+ylim(ax1, [0 60]);
+
+ax2 = axes(t);
+b = bar(ax2, [precip_event.start_time(ind_jan_event) precip_event.end_time(ind_jan_event)], precip_event.total_precip(ind_jan_event),'FaceColor','b','EdgeColor','b','barwidth',20);
+ax2.XAxisLocation = 'top';
+ax2.YAxisLocation = 'right';
+set(ax2, 'YDir', 'reverse');
+set(ax2, 'Xtick', []);
+xlim(ax2, [datenum('12-31-2012 23:59:59') datenum('02-01-2013 00:00:00')]);
+ylabel(ax2, 'Total Precipitation (cm)')
+ax2.Color = 'none';
+ax1.Box = 'off';
+ax2.Box = 'off';
+title(t, 'Total Precipitation and PM 2.5 Concentration in Los Angeles in Janurary 2013')
